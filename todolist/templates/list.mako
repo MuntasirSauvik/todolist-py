838d2609a47f8491f4d1cb619d27d00122da77b8
Muntasir Sauvik <muntasir.sauvik@cpdist.ca>
2020-11-20 15:21:21 -0600

    Implemented the mark complete route

--- todolist/templates/list.mako
+++ todolist/templates/list.mako
@@ -1,11 +1,22 @@
 <%inherit file="common.mako"/>
 
-<div class="box" id="heading">
-${listName}
-</div>
 
 <div class="box">
 
+    <div class="box" id="heading">
+        ${listName}
+    </div>
+
+    % for i in res:
+      <form action="/markComplete" method="post">
+        <p class="${i.completed and 'completed' or ''}">
+            <input type="checkbox" name="completed" onChange="this.form.submit()" ${i.completed and 'checked' or ''}>
+            <span>${i.item_text}</span>
+            <span>${i.completed and 'completed' or ''}</span>
+        </p>
+        <input type="hidden" name="itemId" value="${i.id}"/>
+      </form>
+    % endfor
   <form class="item" action="/addItem" method="post">
     <input type="text" name="newItem" placeholder="New Item" autocomplete="off">
     <button class="btn1" type="submit" name="list" >+</button>
--- todolist/views/lists.py
+++ todolist/views/lists.py
@@ -7,6 +7,7 @@ import transaction
 
 from .. import models
 
+
 @view_config(route_name='add_item')
 def add_item(request):
     item = request.params['newItem']
@@ -20,8 +21,31 @@ def add_item(request):
 
 
 @view_config(route_name='list_name')
-def list_items(request):
+def list_name(request):
     listName = request.matchdict['listName']
     normalizeName = listName.lower()
-    data = {"listName": listName}
+    data = {
+                "listName": listName,
+                "res": request.dbsession.query(models.Item).all()
+            }
     return render_to_response("todolist:templates/list.mako", data, request=request)
+
+
+@view_config(route_name='mark_complete')
+def mark_complete(request):
+    item_id = request.params['itemId']
+    completed = request.params['completed']
+    item = request.dbsession.query(models.Item).filter_by(id=item_id).one()
+    item.completed = bool(completed)
+    request.dbsession.flush()
+    return HTTPFound(location=request.route_url('home'))
+
+
+@view_config(route_name='delete')
+def delete(request):
+    item_id = request.params['itemId']
+    completed = request.params['completed']
+    item = request.dbsession.query(models.Item).filter_by(id=item_id).one()
+    item.completed = bool(completed)
+    request.dbsession.flush()
+    return HTTPFound(location=request.route_url('home'))
